ca3d2b92b45da6d1e6644ce7d311bd8e83f9590f
log1k <log1k@mail.ru>
2018-11-27 21:16:10 +0300

    dz2a

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ﻿http://127.0.0.1:3000/
 
-Запуск:
+DZ2a запуск:
 
-node server.js
+npm run start
+
+DZ2b запуск:
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
+const { INTERVAL_SHOW_DATA, STOP_SHOW_DATA } = process.env;
 //Переменные окружения
 //require('dotenv').config(); npm i dotenv и создать файл .env с переменными или через scripts в package.json, предварительно установив кроссплатформенный пакет env с Npm(cross-env)
 
@@ -12,12 +13,12 @@ http.createServer((req, res) => {
     }
     let showTimeUTC = setInterval(() => {
         console.log(new Date().getTime().toString());
-    }, 1000);
+    }, INTERVAL_SHOW_DATA);
 
     setTimeout(() => {
         clearInterval(showTimeUTC);
         res.end(new Date().getTime().toString());
-    }, 5000);
+    }, STOP_SHOW_DATA);
 }).listen(3000, '127.0.0.1', () => {
     console.log('Сервер начал прослушивание запросов');
 });
