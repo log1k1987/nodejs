868d6baaab43bf5d662bc80a73ac6fcb65930d7c
log1k <log1k@mail.ru>
2018-12-02 21:22:36 +0300

    upload

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -4,18 +4,22 @@ module.exports = function() {
   const patterns = [];
   $.gulp.task('pug', function() {
     patterns.push({ match: '%=suffix=%', replace: $.dev ? '' : '.min' });
-    patterns.push({ match: '%=version=%', replace: $.dev ? '' : `?rel=${$.package.version}` });//Math.ceil(Math.random()*100000)
+    patterns.push({ match: '%=version=%', replace: $.dev ? '' : `?rel=${$.package.version}` }); //Math.ceil(Math.random()*100000)
 
-    return $.gulp.src('./source/template/pages/*.pug')
+    return $.gulp
+      .src('./source/views/pages/*.pug')
       .pipe($.gp.pug({ pretty: true }))
-      .on('error', $.gp.notify.onError(function(error) {
-        return {
-          title: 'Pug',
-          message:  error.message
-        };
-      }))
+      .on(
+        'error',
+        $.gp.notify.onError(function(error) {
+          return {
+            title: 'Pug',
+            message: error.message,
+          };
+        })
+      )
       .pipe($.gp.replaceTask({ patterns, usePrefix: false }))
       .pipe($.gulp.dest($.config.root))
-      .pipe($.browserSync.stream({once: true}));
+      .pipe($.browserSync.stream({ once: true }));
   });
 };
--- source/routes/index.js
+++ source/routes/index.js
@@ -9,6 +9,9 @@ router.get('/', ctrlHome.getIndex);
 router.post('/', ctrlHome.sendData);
 
 router.get('/login', ctrlLogin.getLogin);
+
 router.get('/admin', ctrlAdmin.getAdmin);
+router.post('/admin/skills', ctrlAdmin.sendSkills);
+router.post('/admin/upload', ctrlAdmin.sendUpload);
 
 module.exports = router;
